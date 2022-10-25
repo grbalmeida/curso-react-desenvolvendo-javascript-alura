31cc7fd35931532c572494feeb629a6f42498f89
grbalmeida <g.r.almeida@live.com>
2022-10-24 23:31:46 -0300

    Campo de texto

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,9 +1,13 @@
-import Banner from './componentes/Banner/Banner';
+import Banner from './componentes/Banner';
+import CampoTexto from './componentes/CampoTexto';
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <CampoTexto label="Nome" />
+      <CampoTexto label="Cargo" />
+      <CampoTexto label="Imagem" />
     </div>
   );
 }
---
+++ organo/src/componentes/CampoTexto/index.js
@@ -0,0 +1,12 @@
+import './CampoTexto.css';
+
+const CampoTexto = () => {
+    return (
+        <div className="campo-texto">
+            <label>Nome</label>
+            <input placeholder="Digite o seu nome" />
+        </div>
+    )
+}
+
+export default CampoTexto;
