aa8bc7669badf139136d26668e027e2b7170095b
grbalmeida <g.r.almeida@live.com>
2022-10-29 22:04:31 -0300

    Criando o formulário

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,13 +1,11 @@
 import Banner from './componentes/Banner';
-import CampoTexto from './componentes/CampoTexto';
+import Formulario from './componentes/Formulario';
 
 function App() {
   return (
     <div className="App">
       <Banner />
-      <CampoTexto label="Nome" placeholder="Digite seu nome" />
-      <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
-      <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" />
+      <Formulario />
     </div>
   );
 }
