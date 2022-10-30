35be464e5d14ed6075ddeb7e2bb9f3af50f4cee3
grbalmeida <g.r.almeida@live.com>
2022-10-29 23:02:44 -0300

    Criando o botão

--- organo/src/componentes/Formulario/index.js
+++ organo/src/componentes/Formulario/index.js
@@ -1,5 +1,6 @@
 import CampoTexto from "../CampoTexto";
 import ListaSuspensa from "../ListaSuspensa";
+import Botao from "../Botao";
 
 import './Formulario.css';
 
@@ -23,6 +24,9 @@ const Formulario = () => {
                 <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
                 <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" />
                 <ListaSuspensa label="Time" itens={times} />
+                <Botao>
+                    Criar Card
+                </Botao>
             </form>
         </section>
     )
